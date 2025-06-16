51813cae4cd0f58f0567c698f550822e1b88cf5a
brendan.cohen <brendanintech@gmail.com>
2025-06-15 20:11:30 -0400

    delete

--- src/components/Gravatar.js
+++
@@ -1,6 +0,0 @@
-import md5 from 'md5';
-
-export function getGravatarUrl(email) {
-  const hash = md5(email.trim().toLowerCase());
-  return `https://www.gravatar.com/avatar/${hash}?d=identicon`;
-} 
